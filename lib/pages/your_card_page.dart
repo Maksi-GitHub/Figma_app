2d4c76e62e34f142a2930b4c84de5b3a0e1ad851
Maksim <maks-vasilev-2017@inbox.ru>
2024-03-07 19:27:22 +0500

    Доделан скролл карты

--- lib/pages/your_card_page.dart
+++ lib/pages/your_card_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_app_figma/design/color.dart';
 import 'package:flutter_app_figma/design/icons.dart';
 import 'package:flutter_app_figma/design/your_cards.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class YourCard extends StatefulWidget {
   const YourCard({super.key});
@@ -12,11 +13,15 @@ class YourCard extends StatefulWidget {
 }
 
 class _YourCardState extends State<YourCard> {
+final _controller = PageController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
+        backgroundColor: backgroundColor,
         automaticallyImplyLeading: false,
         title: Padding(
           padding: const EdgeInsets.only(left: 30),
@@ -36,88 +41,107 @@ class _YourCardState extends State<YourCard> {
           )
         ],
       ),
-      body: Container(
-        height: 236,
-        width: 419,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: List.generate(yourCard.length, (index) {
-              return Stack(
-                children: <Widget>[
-                  Image(image: AssetImage(yourCard[index]['card'])),
-                  Positioned(
-                    top: 25,
-                    left: 30,
-                    child: Text(
-                      yourCard[index]['nameCard'],
-                      style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                            fontSize: 20,
-                            color: textColor2,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 120,
-                    left: 75,
-                    child: Text(
-                      yourCard[index]['numberCard'],
-                      style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                            fontSize: 20,
-                            color: textColor2,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 180,
-                    left: 50,
-                    child: Column(
-                      children: [
-                        Text('CARD HOLDER', style: GoogleFonts.inter(textStyle: const TextStyle(fontSize: 8,
-                                color: textColor2,
-                                fontWeight: FontWeight.w400))),
-                        Text(
-                          yourCard[index]['nameUser'],
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                                fontSize: 10,
-                                color: textColor2,
-                                fontWeight: FontWeight.w500),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 236,
+            width: 419,
+            child: PageView(     
+              controller: _controller,
+              scrollDirection: Axis.horizontal,
+                children: List.generate(yourCard.length, (index) {
+                  return Center(
+                    child: Stack(
+                      children: <Widget>[
+                        Image(image: AssetImage(yourCard[index]['card'])),
+                        Positioned(
+                          top: 25,
+                          left: 30,
+                          child: Text(
+                            yourCard[index]['nameCard'],
+                            style: GoogleFonts.inter(
+                              textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  color: textColor2,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 120,
+                          left: 75,
+                          child: Text(
+                            yourCard[index]['numberCard'],
+                            style: GoogleFonts.inter(
+                              textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  color: textColor2,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 180,
+                          left: 50,
+                          child: Column(
+                            children: [
+                              Text('CARD HOLDER', style: GoogleFonts.inter(textStyle: const TextStyle(fontSize: 8,
+                                      color: textColor2,
+                                      fontWeight: FontWeight.w400))),
+                              Text(
+                                yourCard[index]['nameUser'],
+                                style: GoogleFonts.inter(
+                                  textStyle: const TextStyle(
+                                      fontSize: 10,
+                                      color: textColor2,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                          top: 180,
+                          right: 60,
+                          child: Column(
+                            children: [
+                              Text('EXPIRED', style: GoogleFonts.inter(textStyle: const TextStyle(fontSize: 8,
+                                      color: textColor2,
+                                      fontWeight: FontWeight.w400))),
+                              Text(
+                                yourCard[index]['dateCard'],
+                                style: GoogleFonts.inter(
+                                  textStyle: const TextStyle(
+                                      fontSize: 10,
+                                      color: textColor2,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  Positioned(
-                    top: 180,
-                    right: 60,
-                    child: Column(
-                      children: [
-                        Text('EXPIRED', style: GoogleFonts.inter(textStyle: const TextStyle(fontSize: 8,
-                                color: textColor2,
-                                fontWeight: FontWeight.w400))),
-                        Text(
-                          yourCard[index]['dateCard'],
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                                fontSize: 10,
-                                color: textColor2,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            })
+                  );
+                }) 
+            ),
           ),
-        ),
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 4,
+            effect: ScaleEffect(
+              activeDotColor: primaryColor,
+              dotColor: primaryColor.withOpacity(0.3),
+              dotHeight: 3
+            ),),
+
+            Container(
+              )
+        ],
       ),
     );
   }
 }
+
+
+
